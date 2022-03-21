92d46a9a6165c9227aff96e9f813af99adec1a7d
Julio Nunes <juliocraft51@gmail.com>
2022-03-21 18:16:05 -0300

    feat: routes e midd

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const checkListRouter = require('./src/routes/checklist')
+const app = express()
+
+
+
+app.use('/checklist',checkListRouter)
+
+
+app.listen(3002, ()=>{
+    console.log('servidor ativo!')
+})
+
---
+++ src/routes/checklist.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+
+
+const router = express.Router()
+
+
+router.get('/',(req,res)=>{
+    console.log('olá')
+    res.send() 
+})
+
+router.post('/', (req,res)=>{
+console.log(req.body)
+res.status(200).send(req.body)
+})
+
+router.get('/:id', (req,res)=>{
+    console.log(req.params.id)
+    res.send(`ID: ${req.params.id}`)
+})
+
+router.put('/:id', (req,res)=>{
+    console.log(req.params.id)
+    res.send(`PUT ID: ${req.params.id}`)
+})
+
+router.delete('/:id', (req,res)=>{
+    console.log(req.params.id)
+    res.send(`Delete ID: ${req.params.id}`)
+})
+
+
+module.exports = router
